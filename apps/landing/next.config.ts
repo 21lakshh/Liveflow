ae80854aed219c6e718bacb3fcf8edec0194a2e9
21lakshh <lakshyapaliwalodd@gmail.com>
2026-03-10 22:59:11 +0530

    added video demo

--- apps/landing/next.config.ts
+++ apps/landing/next.config.ts
@@ -1,7 +1,23 @@
+import { withNextVideo } from "next-video/process";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.scdn.co',
+        port: '',
+        pathname: '/**', // Allow all paths from this domain
+      },
+      {
+        protocol: 'https',
+        hostname: 'api.microlink.io', // <--- ADD THIS BLOCK
+        port: '',
+        pathname: '/**',
+      },
+    ]
+  },
 };
 
-export default nextConfig;
+export default withNextVideo(nextConfig);
